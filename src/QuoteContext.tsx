5149deef3ff5612402671c655a1152442eac8f5d
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2022-10-15 00:16:00 +0700

    fix bug not build

--- src/QuoteContext.tsx
+++ src/QuoteContext.tsx
@@ -1,11 +1,11 @@
-import { createContext } from "react";
+import { createContext } from 'react';
 export type QuoteType = {
    id: number;
    author: string;
    description: string;
 };
 const QuoteContext = createContext({
-   quotes: [{ id: 1, author: "", description: "" }],
+   quotes: [{ id: 1, author: '', description: '' }],
    currQuote: 1,
    setCurrQuote: (value: number) => {},
 });
